a38fb8f41df355135affefd48ee689d892488c22
Tomek <tomas@emocio.cz>
2026-04-03 21:21:22 +0300

    v3.28: New banana favicon by An Nguyen (CC BY), replace emoji icon everywhere

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'bananovnik-v3.18';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE = 'bananovnik-v3.28';
+const ASSETS = ['./', './index.html', './manifest.json', './favicon.svg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));

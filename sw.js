fc5b94a38b52d21f93a2b9b8406b478757a31cd9
Tomek <tomas@emocio.cz>
2026-04-03 23:01:21 +0300

    v4.13: Fix pull-hint showing without new version, neutral pull-to-refresh text

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bananovnik-v4.12';
+const CACHE = 'bananovnik-v4.13';
 const ASSETS = ['./', './index.html', './manifest.json', './favicon.svg', './icon-180.png'];
 
 self.addEventListener('install', e => {

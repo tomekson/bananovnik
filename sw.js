f46d4cdbfea76f83d408abd2454f15d397ebae67
Tomek <tomas@emocio.cz>
2026-04-03 22:53:34 +0300

    v4.12: Abort dialog full offset, stronger danger button, green continue button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bananovnik-v4.11';
+const CACHE = 'bananovnik-v4.12';
 const ASSETS = ['./', './index.html', './manifest.json', './favicon.svg', './icon-180.png'];
 
 self.addEventListener('install', e => {

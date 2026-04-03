64c52b284b921d43595565f07442ccd91c5a2b5a
Tomek <tomas@emocio.cz>
2026-04-03 22:24:01 +0300

    v4.00: Dazzle offset on quiz answers, auto-scroll after confirm, btn-retry offset, text shadows

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bananovnik-v3.30';
+const CACHE = 'bananovnik-v4.00';
 const ASSETS = ['./', './index.html', './manifest.json', './favicon.svg', './icon-180.png'];
 
 self.addEventListener('install', e => {

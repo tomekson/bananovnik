74046621f2edb7ca4afddd52d6d8ab936ccdc8b1
Tomek <tomas@emocio.cz>
2026-04-03 22:40:52 +0300

    v4.10: Yellow brand colors for quiz answers, progress bar gradient, hint button, neutral selection checkbox

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bananovnik-v4.00';
+const CACHE = 'bananovnik-v4.10';
 const ASSETS = ['./', './index.html', './manifest.json', './favicon.svg', './icon-180.png'];
 
 self.addEventListener('install', e => {

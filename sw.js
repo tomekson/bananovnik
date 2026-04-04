9cdaa0deb2b01b963ad194dc49eb8d0a65f59ce6
Tomek <tomas@emocio.cz>
2026-04-04 21:12:14 +0300

    Bump version to 4.20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bananovnik-v4.14';
+const CACHE = 'bananovnik-v4.20';
 const ASSETS = ['./', './index.html', './manifest.json', './favicon.svg', './icon-180.png'];
 
 self.addEventListener('install', e => {

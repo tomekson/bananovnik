3899d32b83b88f8e3758115b6b8478d51c560ec8
Tomek <tomas@emocio.cz>
2026-04-04 22:50:51 +0300

    v4.33: theme-color #F3EDC4 everywhere

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bananovnik-v4.32';
+const CACHE = 'bananovnik-v4.33';
 const ASSETS = ['./', './index.html', './manifest.json', './favicon.svg', './icon-180.png'];
 
 self.addEventListener('install', e => {

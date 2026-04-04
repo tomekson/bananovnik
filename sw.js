d11160cf76f5d2622c73dcd45721b8740631e83f
Tomek <tomas@emocio.cz>
2026-04-04 22:46:13 +0300

    v4.32: Dynamic theme-color animation yellow→cream, fix status bar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bananovnik-v4.31';
+const CACHE = 'bananovnik-v4.32';
 const ASSETS = ['./', './index.html', './manifest.json', './favicon.svg', './icon-180.png'];
 
 self.addEventListener('install', e => {

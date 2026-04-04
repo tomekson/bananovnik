002f949e826583c960a4199e122ebc7a4381c299
Tomek <tomas@emocio.cz>
2026-04-04 21:34:27 +0300

    v4.26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bananovnik-v4.25';
+const CACHE = 'bananovnik-v4.26';
 const ASSETS = ['./', './index.html', './manifest.json', './favicon.svg', './icon-180.png'];
 
 self.addEventListener('install', e => {

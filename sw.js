bfa620414fa2bc0d8aa5a32f80c593548c2755b1
Tomek <tomas@emocio.cz>
2026-04-05 00:09:37 +0300

    v4.45: Gradient fade above quiz footer, hide Android install text

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bananovnik-v4.44';
+const CACHE = 'bananovnik-v4.45';
 const ASSETS = ['./', './index.html', './manifest.json', './favicon.svg', './icon-180.png'];
 
 self.addEventListener('install', e => {

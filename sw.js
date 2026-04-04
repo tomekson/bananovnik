f69061b733c38c879a454902369ce786916d85cb
Tomek <tomas@emocio.cz>
2026-04-04 22:02:51 +0300

    v4.28: Fix iOS status bar - black-translucent, manifest cache-bust

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bananovnik-v4.27';
+const CACHE = 'bananovnik-v4.28';
 const ASSETS = ['./', './index.html', './manifest.json', './favicon.svg', './icon-180.png'];
 
 self.addEventListener('install', e => {

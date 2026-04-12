29e97780f75a3f17fc6971d93e15d25c481a5191
Tomek <tomas@emocio.cz>
2026-04-12 22:41:23 +0200

    v4.60: Scroll hint, keyboard shortcuts, retry reshuffle, scroll-to-option, orig question number, new question Haptika

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bananovnik-v4.45';
+const CACHE = 'bananovnik-v4.60';
 const ASSETS = ['./', './index.html', './manifest.json', './favicon.svg', './icon-180.png'];
 
 self.addEventListener('install', e => {

867fd9e345fda7b62bdc0c97f729080c755ca8d9
Tomek <tomas@emocio.cz>
2026-04-25 13:45:14 +0200

    v5.21: logo vlevo na dashboardu i detailu testu, logo v detailu jako odkaz zpět na testy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bananovnik-v5.20';
+const CACHE = 'bananovnik-v5.21';
 const CORE = ['./', './index.html', './manifest.json', './favicon.svg', './icon-180.png'];
 const TEST_FILES = ['./tests/komunikace.json', './tests/management.json', './tests/management-stare.json'];
 

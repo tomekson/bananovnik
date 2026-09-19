1f845183d3968edb62e41ed6b913c91123aa6178
Tomek <tomas@emocio.cz>
2026-09-19 13:58:47 +0200

    v9.39 - nový test Anglický jazyk (splněno): 247 otázek z obou částí výstupního testu na Primátu (1. a 2. část po 125 otázkách, 3 duplicity odstraněny), 21 gramatických sad od nejlehčích po nejtěžší, podmínky zkoušky (45 min., 90 otázek, 100 bodů, hranice 51). Klíč podle školy; 5 sporných otázek „bez záruky" (2 s doplněnou druhou správnou odpovědí podle částečných bodů, 2 se sporným klíčem školy). Test je označen jako splněný a řadí se hned za nesplněné.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'bananovnik-v9.38';
+const CACHE = 'bananovnik-v9.39';
 const CORE = ['./', './index.html', './manifest.json', './favicon.svg', './icon-180.png'];
-const TEST_FILES = ['./tests/komunikace.json', './tests/management.json', './tests/management-stare.json', './tests/manazerske-dovednosti.json'];
+const TEST_FILES = ['./tests/komunikace.json', './tests/management.json', './tests/management-stare.json', './tests/manazerske-dovednosti.json', './tests/anglicky-jazyk.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(
